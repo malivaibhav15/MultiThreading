300a609d9e68d128001168012f8a3ed854471a00
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-14 12:53:26 +0530

    ADDED : Pop method for stack pop

--- src/main/java/com/BridgeLabz/stack/Pop.java
+++ src/main/java/com/BridgeLabz/stack/Pop.java
@@ -2,12 +2,38 @@ package com.BridgeLabz.stack;
 
 import java.util.Stack;
 
-public class Pop extends PushNPop implements Runnable
-{
+public class Pop extends PushNPop implements Runnable {
 
     @Override
     public void run() {
-       
+
+        stack.push("AB");
+        printStack(stack);
+        stack.push("CD");
+        printStack(stack);
+        stack.push("EF");
+        printStack(stack);
+        stack.push("GH");
+        printStack(stack);
+        stack.push("IJ");
+        printStack(stack);
+        stack.push("KL");
+        printStack(stack);
+        stack.push("YZ");
+        printStack(stack);
+
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
     }
 
     private void printStack(Stack<String> stack) {
