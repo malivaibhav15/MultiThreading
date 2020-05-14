4c4f68d0fabcc0c415cf19f512f29dd2aac8bb37
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-14 12:53:47 +0530

    ADDED : Push method for stack push

--- src/main/java/com/BridgeLabz/stack/Push.java
+++ src/main/java/com/BridgeLabz/stack/Push.java
@@ -6,12 +6,39 @@ public class Push extends PushNPop implements Runnable {
     @Override
     public void run() {
 
+        stack.push("AB");
+        printStack(stack);
+        stack.push("CD");
+        printStack(stack);
+        stack.push("EF");
+        printStack(stack);
+        stack.push("GH");
+        printStack(stack);
+        stack.push("IJ");
+        printStack(stack);
+        stack.push("KL");
+        printStack(stack);
+        stack.push("YZ");
+        printStack(stack);
+
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
     }
 
     private void printStack(Stack<String> stack) {
         if (stack.isEmpty())
             System.out.println("Stack is empty");
-        System.out.println(stack.peek());
+        System.out.println(stack);
     }
 
 }
